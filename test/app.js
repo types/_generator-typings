7d8dc542b07d65b76c51d7bacd4eb6f80f246a63
Homa Wong <homawong@gmail.com>
2016-03-01 10:40:47 -0800

    Increase timeout
    To see if that help travis to pass older node versions.

--- test/app.js
+++ test/app.js
@@ -3,15 +3,18 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
-describe('generator-typings:app', function () {
+describe('app', function () {
   before(function (done) {
-    this.timeout(30000);
+    this.timeout(60000);
     helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({
-        sourceUri: 'facebook/reactDOM',
-        projectUri: 'unional/typed-reactDOM',
+        sourceUri: 'visionmedia/batch',
+        projectUri: 'typed-typings/npm-batch',
+        // Set to false to avoid longer test time.
         isNpm: false,
-        username: 'unional'
+        username: 'unional',
+        license: 'MIT',
+        name: 'unional'
       })
       .on('end', done);
   });
