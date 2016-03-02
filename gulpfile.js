3b7094cf8de05638fc6b14b9f69748ad5a943950
Homa Wong <homawong@gmail.com>
2016-03-02 14:04:59 -0800

    Scope `gulp static` to source code only
    `**/*.js` includes `node_modules`. Wasted time

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
+  return gulp.src('generators/**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
