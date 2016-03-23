3ba0b228e16d4d3996ee32336fc157eab17a74e3
Homa Wong <homawong@gmail.com>
2016-03-23 15:26:28 -0700

    Remove ambient test for now
    It doesn't do much and really slows down publish

--- test/app.js
+++ test/app.js
@@ -40,40 +40,4 @@ describe('app', function() {
       ]);
     });
   });
-  describe('namespace', function() {
-    before(function(done) {
-      this.timeout(60000);
-      helpers.run(path.join(__dirname, '../generators/app'))
-        .withPrompts({
-          sourceUri: 'visionmedia/batch',
-          projectUri: 'typed-typings/npm-batch',
-          // Set to false to avoid longer test time.
-          isNpm: false,
-          isAmbient: true,
-          username: 'unional',
-          license: 'MIT',
-          name: 'unional'
-        })
-        .on('end', done);
-    });
-
-    it('creates files', function() {
-      assert.file([
-        '.vscode/settings.json',
-        'out/main.d.ts',
-        'out/browser.d.ts',
-        'typings/main.d.ts',
-        'typings/browser.d.ts',
-        'test/test.ts',
-        '.editorconfig',
-        '.gitignore',
-        'LICENSE',
-        'index.d.ts',
-        'README.md',
-        'package.json',
-        'typings.json',
-        'tsconfig.json'
-      ]);
-    });
-  });
 });
