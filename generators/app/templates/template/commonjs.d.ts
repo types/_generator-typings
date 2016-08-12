a0b284eb055b995005980b32b96cf3e01ecafca6
Homa Wong <homawong@gmail.com>
2016-08-12 13:51:07 -0700

    Comment (#131)
    * 1.0.12
    
    * add template comment

--- generators/app/templates/template/commonjs.d.ts
+++ generators/app/templates/template/commonjs.d.ts
@@ -1,3 +1,8 @@
+/**
+ * This is just a template.
+ * If the source does not export function at top level,
+ * you can skip the declare namespace and export what's inside directly.
+ */
 declare namespace <%- name %> {
 
 }
--- generators/app/templates/template/global.d.ts
+++ generators/app/templates/template/global.d.ts
@@ -1,3 +1,7 @@
+/**
+ * This is just a template.
+ * Feel free to not using this construct depends on how the source package is defined.
+ */
 declare namespace <%- name %> {
 
 }
