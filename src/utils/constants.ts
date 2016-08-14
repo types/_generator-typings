b292b9384e8dc641756428f39186047210541ccf
Homa Wong <homawong@gmail.com>
2016-08-13 20:38:20 -0700

    rename to typesrepo

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 const pkg = require('../../package.json')
 
-export const PROJECT_NAME = 'typesgen'
-export const PRETTY_PROJECT_NAME = 'TypesGen'
+export const PROJECT_NAME = 'typesrepo'
+export const PRETTY_PROJECT_NAME = 'TypesRepo'
 export const CONFIG_FILE = `${PROJECT_NAME}.json`
 export const RC_FILE = `${PROJECT_NAME}.rc`
 export const VERSION = pkg.version
