8347d2b461a2f286650d4a60c5e5581da1b7383e
Homa Wong <homawong@gmail.com>
2016-08-18 01:14:49 -0700

    Improve watch script

--- scripts/watch.js
+++ scripts/watch.js
@@ -3,16 +3,18 @@
 const cp = require('child_process');
 
 let compiling = true;
-cp.spawn('tsc', ['-w'])
+cp.spawn('tsc', ['-w'], { shell: true })
   .stdout.on('data', data => {
     console.log(data.toString())
     if (compiling) {
       compiling = false;
       cp.spawn('ava', ['-w', process.argv[2]], {
-        stdio: 'inherit'
+        stdio: 'inherit',
+        shell: true
       });
     }
     cp.spawnSync('npm', ['run', 'lint'], {
-      stdio: 'inherit'
+      stdio: 'inherit',
+      shell: true
     })
   })
