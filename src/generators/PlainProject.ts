59a924935fd9d589f71a3cc2f3e28ebeb47065ee
Homa Wong <homawong@gmail.com>
2017-07-22 21:18:05 -0700

    Backup changes

--- src/generators/PlainProject.ts
+++ src/generators/PlainProject.ts
@@ -1,12 +1,19 @@
 import { paramCase } from 'change-case'
 import { execSync } from 'child_process'
+import inquirer = require('inquirer')
+import chalk = require('chalk')
 import { resolve, join } from 'path'
 
 import { GeneratorContext, Generator } from '../utils/Generator'
 import { CLI_VERSION } from '../utils/constants'
-import { CommandOptions, ParsedArgs } from '../utils/command'
+import { CommandOptions } from '../utils/command'
 
-export class PlainProject extends Generator {
+export interface PlainProjectGeneratorContext extends GeneratorContext {
+  licnse: string
+  sourceUsages: string[]
+}
+
+export class PlainProject extends Generator<PlainProjectGeneratorContext> {
   static commandOptions: CommandOptions = {
     boolean: {
     },
@@ -26,24 +33,22 @@ export class PlainProject extends Generator {
   packageName: string
   packageBundleFilename: string
   license: string
-  context: GeneratorContext
 
-  constructor(log, fs, store) {
-    super(log, fs, store)
+  constructor(log, fs, store, context, argv) {
+    super(log, fs, store, context, argv)
   }
 
-  initializing(context: GeneratorContext, argv: ParsedArgs) {
-    this.context = context
-    this.license = argv.license
+  initializing() {
+    this.license = this.argv.license
     this.log.debug(`license: ${this.license}`)
 
     try {
-      const repoUrl = execSync(`npm info ${context.projectName} repository.url`).toString()
-      this.packageName = repoUrl.indexOf(`${context.organization}/${context.projectName}`) === -1 ?
-        `@${context.organization}/${context.projectName}` : context.projectName
+      const repoUrl = execSync(`npm info ${this.context.projectName} repository.url`).toString()
+      this.packageName = repoUrl.indexOf(`${this.context.organization}/${this.context.projectName}`) === -1 ?
+        `@${this.context.organization}/${this.context.projectName}` : this.context.projectName
     }
     catch (e) {
-      this.packageName = context.projectName
+      this.packageName = this.context.projectName
     }
     this.log.debug(`packageName: ${this.packageName}`)
 
@@ -51,7 +56,32 @@ export class PlainProject extends Generator {
     this.log.debug(`packageBundleFilename: ${this.packageBundleFilename}`)
   }
   prompting() {
-    // TODO
+    return inquirer.prompt([{
+      type: 'checkbox',
+      name: 'usages',
+      message: `${chalk.green('How')} can the package be used?`,
+      choices: [
+        { name: 'AMD Module', value: 'amd' },
+        { name: 'CommonJS Module', value: 'commonjs', checked: true },
+        { name: 'ES2015 Module', value: 'esm' },
+        { name: 'Script Tag', value: 'script' },
+        { name: 'part of environment', value: 'env' }
+      ],
+      validate: (values) => values.length > 0
+    }, {
+      type: 'checkbox',
+      name: 'platforms',
+      message: `${chalk.green('Where')} can the package be used?`,
+      choices: [
+        { name: 'Browser', value: 'browser' },
+        { name: 'Native NodeJS', value: 'node', checked: true },
+        { name: 'others (e.g. atom)', value: 'others' }
+      ],
+      validate: (values) => values.length > 0,
+      default: []
+    }]).then((props) => {
+      this.context.sourceUsages = props.usages as any
+    })
   }
   writing() {
     const { projectFolder } = this.context
@@ -81,9 +111,9 @@ export class PlainProject extends Generator {
       join(templatePath, 'package.json'),
       join(projectFolder, 'package.json'),
       {
-        ambient: !(~this.props.sourceUsages.indexOf('commonjs') ||
-          ~this.props.sourceUsages.indexOf('amd') ||
-          ~this.props.sourceUsages.indexOf('esm')) ? ' --ambient' : '',
+        ambient: !(~this.context.sourceUsages.indexOf('commonjs') ||
+          ~this.context.sourceUsages.indexOf('amd') ||
+          ~this.context.sourceUsages.indexOf('esm')) ? ' --ambient' : '',
         packagename: this.packageName,
         packageBundleFilename: this.packageBundleFilename,
         authorName: this.context.authorName,
